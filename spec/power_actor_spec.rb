88eff96f3c4845f6a9c4f17e7de4a32e6dcc16c2
Adam Gamble <adamgamble@gmail.com>
2012-11-02 15:44:52 -0600

    100% test coverage

--- spec/power_actor_spec.rb
+++ spec/power_actor_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Actor::Power do
   before do
-    @actor = Actor::Power.new(mock)
+    @map = mock "map"
+    @actor = Actor::Power.new(@map)
   end
 
   it "offers power" do
@@ -11,5 +12,17 @@ describe Actor::Power do
 
   it "doesn't offer water" do
     @actor.offer?(:water).should be_false
+  end
+
+  it "moves north on tick" do
+    map_cell = mock "map_cell"
+    other_map_cell = mock "other_map_cell"
+    point = Map::Point.new(1,1)
+    map_cell.should_receive(:point).and_return(point)
+    map_cell.should_receive(:delete).with(@actor)
+    other_map_cell.should_receive(:<<).with(@actor)
+    @map.should_receive(:cell_at).with(Map::Point.new(1,0)).and_return(other_map_cell)
+    @map.should_receive(:cell_for_object).and_return(map_cell)
+    @actor.tick
   end
 end
