4762bc72142e9f667b42a5c9147c8881f936888c
Adam Gamble <adamgamble@gmail.com>
2012-11-03 14:47:45 -0600

    refactored test to more closely match the class heirarchy, also I don't know how to spell heirachy

---
+++ spec/resource_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe Actor::Resource do
+  before do
+    @map = mock "map"
+    @actor = Actor::Resource.new(@map)
+  end
+
+  it "moves to an adjacent road if available on tick" do
+    fake_cell = mock 'fake cell'
+    other_fake_cell = mock 'other fake cell'
+    fake_road = mock "fake road"
+
+    map_cell_north = mock "map_cell_north"
+    map_cell_south = mock "map_cell_south"
+    map_cell_east = mock "map_cell_east"
+    map_cell_west = mock "map_cell_west"
+    [map_cell_north, map_cell_south, map_cell_east].each { |obj| obj.should_receive(:detect).and_return(nil) }
+    map_cell_west.should_receive(:detect).exactly(1).times.and_return(fake_road)
+    @map.should_receive(:neighbors_for_object).with(@actor).and_return([map_cell_north, map_cell_south, map_cell_east, map_cell_west])
+    @map.should_receive(:cell_for_object).with(fake_road).and_return(fake_cell)
+    @map.should_receive(:cell_for_object).with(@actor).and_return(other_fake_cell)
+    other_fake_cell.should_receive(:delete).with(@actor)
+    fake_cell.should_receive(:<<).with(@actor)
+    @actor.tick
+  end
+
+  it "deletes itself if there is no road" do
+    other_fake_cell = mock 'other fake cell'
+
+    map_cell_north = mock "map_cell_north"
+    map_cell_south = mock "map_cell_south"
+    map_cell_east = mock "map_cell_east"
+    map_cell_west = mock "map_cell_west"
+    [map_cell_north, map_cell_south, map_cell_east, map_cell_west].each { |obj| obj.should_receive(:detect).and_return(nil) }
+    @map.should_receive(:neighbors_for_object).with(@actor).and_return([map_cell_north, map_cell_south, map_cell_east, map_cell_west])
+    @map.should_receive(:cell_for_object).with(@actor).and_return(other_fake_cell)
+    other_fake_cell.should_receive(:delete).with(@actor)
+    @actor.tick
+  end
+
+  it "won't even consider going into the cell it was last in" do
+    current_cell = mock 'current_cell'
+
+    map_cell_north = mock "map_cell_north"
+    map_cell_south = mock "map_cell_south"
+    map_cell_east = mock "map_cell_east"
+    map_cell_west = mock "map_cell_west"
+    fake_road = mock "fake road"
+    [map_cell_north, map_cell_south, map_cell_east].each { |obj| obj.should_receive(:detect).and_return(nil) }
+    @map.should_receive(:neighbors_for_object).with(@actor).and_return([map_cell_north, map_cell_south, map_cell_east, map_cell_west])
+    @map.should_receive(:cell_for_object).with(@actor).and_return(current_cell)
+    @actor.last_cell = map_cell_west
+    current_cell.should_receive(:delete).with(@actor)
+    @actor.tick
+  end
+end
--- spec/water_actor_spec.rb
+++ spec/water_actor_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe Actor::Water do
+  before do
+    @water_actor = Actor::Water.new(mock)
+  end
+
   it "should have a resource type" do
-    water_actor = Actor::Water.new(mock)
-    water_actor.resource_type.should == :water
+    @water_actor.resource_type.should == :water
+  end
+
+  it "offers water" do
+    @water_actor.offer?(:water).should be_true
   end
 end
