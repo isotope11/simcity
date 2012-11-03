3fba810d8d1b661bb5936439ed3b00a21aafd8ce
Adam Gamble <adamgamble@gmail.com>
2012-11-03 16:22:25 -0600

    returned to 100% test coverage

--- spec/garbage_dump_spec.rb
+++ spec/garbage_dump_spec.rb
@@ -9,8 +9,18 @@ describe GarbageDump do
     @west_cell = mock "west_cell"
   end
 
-  it "should consume water and power on tick" do
+  it "should consume garbage on tick" do
     @garbage_dump.should_receive(:consume_garbage).exactly(1).times
     @garbage_dump.tick
+  end
+
+  it "consumes garbage if it is nearby" do
+    @garbage = mock "garbage"
+    @north_cell.should_receive(:detect).and_return(@garbage)
+
+    @map.should_receive(:neighbors_for_object).with(@garbage_dump).and_return([@north_cell, @south_cell, @east_cell, @west_cell])
+    @map.should_receive(:cell_for_object).with(@garbage).and_return(@north_cell)
+    @north_cell.should_receive(:delete).with(@garbage)
+    @garbage_dump.consume_garbage
   end
 end
